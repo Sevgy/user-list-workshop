c557697776b5d7eed9741bd67f32eae6d398002a
Sevgy <121260146+Sevgy@users.noreply.github.com>
2023-12-09 03:07:43 +0200

    Add initial project files

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Footer from './components/Footer.jsx';
+import Header from './components/Header.jsx';
+import UserList from './components/UserList.jsx';
+import './styles.css';
+
+function App() {
+
+    return (
+        <div>
+            <Header />
+            <main className="main">
+                <UserList/>
+            </main>
+            <Footer />
+        </div>
+    )
+}
+
+export default App
---
+++ src/components/UserList.jsx
@@ -0,0 +1,15 @@
+import Search from "./Search.jsx";
+import UserListTable from "./UserListTable.jsx";
+
+const UserList = () => {
+    return (
+
+        <section className="card users-container">
+            <Search/>
+
+            <UserListTable/>
+        </section>
+    );
+};
+
+export default UserList
